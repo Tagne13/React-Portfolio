240795b3aac06b79985c768f0e3931ce5917bf87
El Jefe <Tagne13@gmail.com>
2023-06-25 19:13:28 -0400

    added js function to create footer to page

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,10 @@ import './footer.css';
 function Footer () {
     return (
         <footer id='footer'>
-            
+            <div>
+                <h4>Made by Jeff Montagne PT, DPT, ATC</h4>
+                <p>&copy; 2023</p>
+            </div>
         </footer>
     )
 }
