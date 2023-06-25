a43a2465281d75a4bacbc1d316aab1220b56216c
El Jefe <Tagne13@gmail.com>
2023-06-25 18:34:29 -0400

    created app.js file with component imports for structure of page

---
+++ src/components/navigation/nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useSate} from 'react';
+import './nav.css';
+
+function Navigation() {
+    return (
+        <nav>
+            
+        </nav>
+    )
+}
+
+export default Navigation;
