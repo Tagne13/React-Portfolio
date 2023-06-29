6b91779b1dc21023ca430212541c39ec14c3ffa9
El Jefe <Tagne13@gmail.com>
2023-06-29 16:40:25 -0400

    added social media links to footer and styled to page; installed react-icons

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './footer.css';
+import { AiOutlineFacebook } from 'react-icons/ai';
+import { FiGithub, FiLinkedin } from 'react-icons/fi';
 
 function Footer () {
     return (
@@ -7,6 +9,11 @@ function Footer () {
             <div>
                 <h5>Made by Jeff Montagne PT, DPT, ATC</h5>
                 <p>&copy; 2023</p>
+            </div>
+            <div className='footer_links'>
+                <a href='https://www.facebook.com/jeff.montagne.7' target='_blank'><AiOutlineFacebook/></a>
+                <a href='https://github.com/Tagne13' target='_blank'><FiGithub/></a>
+                <a href='https://linkedin.com/in/jeff-montagne-1b5544281' target='_blank'><FiLinkedin/></a>
             </div>
         </footer>
     )
